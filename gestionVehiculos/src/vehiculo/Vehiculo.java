97f37b79ffff5bb94afcab45f4b2020bba29e36c
almaeq <alma.quinterosquiroga@gmail.com>
2025-04-03 15:40:29 -0300

    add getters and setters

--- gestionVehiculos/src/vehiculo/Vehiculo.java
+++ gestionVehiculos/src/vehiculo/Vehiculo.java
@@ -5,4 +5,50 @@ public class Vehiculo {
     private String marca;
     private Integer anio;
     private double capacidadCargaKg;
+
+    public Vehiculo(String patente, String marca, Integer anio, double capacidadCargaKg) {
+        this.patente = patente;
+        this.marca = marca;
+        this.anio = anio;
+        this.capacidadCargaKg = capacidadCargaKg;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getAnio() {
+        return anio;
+    }
+
+    public void setAnio(Integer anio) {
+        this.anio = anio;
+    }
+
+    public double getCapacidadCargaKg() {
+        return capacidadCargaKg;
+    }
+
+    public void setCapacidadCargaKg(double capacidadCargaKg) {
+        this.capacidadCargaKg = capacidadCargaKg;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Patente: " + patente);
+        System.out.println("Marca: " + marca);
+        System.out.println("Anio: " + anio);
+        System.out.println("Capacidad: " + capacidadCargaKg);
+    }
 }
