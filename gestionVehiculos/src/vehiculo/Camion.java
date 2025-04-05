be1693ed750d43b5e8d389774027127c0ced8a97
almaeq <alma.quinterosquiroga@gmail.com>
2025-04-05 08:51:57 -0300

    add camion class

---
+++ gestionVehiculos/src/vehiculo/Camion.java
@@ -0,0 +1,18 @@
+package vehiculo;
+
+public class Camion extends Vehicle {
+    private boolean tieneAcoplado;
+
+    public Camion(String patente, String marca, Integer anio, double capacidadCargaKg) {
+        super(patente, marca, anio, capacidadCargaKg);
+        this.tieneAcoplado = tieneAcoplado;
+    }
+
+    public boolean isTieneAcoplado() {
+        return tieneAcoplado;
+    }
+
+    public void setTieneAcoplado(boolean tieneAcoplado) {
+        this.tieneAcoplado = tieneAcoplado;
+    }
+}
